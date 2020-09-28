d8b6b8b3246d1883698a21c70e2c4177cf8f1296
dathanwong <dwong5@villanova.edu>
2020-09-28 09:27:52 -0700

    Fixed issue where large files were getting blocked

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8000;
 
 require('./server/config/mongoose.config');
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended:true}));
 require('./server/routes/lyric.routes')(app);
 require('./server/routes/selection.routes')(app);
